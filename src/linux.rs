3db16e7f6b28f7f55e7adbc5889bcdcdbd86671f
Funami580 <Funami580@users.noreply.github.com>
2021-11-03 02:59:45 +0100

    Fix check for config files

--- src/linux.rs
+++ src/linux.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use detect_desktop_environment::DesktopEnvironment;
 
 fn is_dark_mode_enabled() -> bool {
@@ -6,21 +8,29 @@ fn is_dark_mode_enabled() -> bool {
         DesktopEnvironment::Cinnamon => check_dconf("/org/cinnamon/desktop/interface/gtk-theme"),
         DesktopEnvironment::Enlightenment => false,
         DesktopEnvironment::Gnome => check_dconf("/org/gnome/desktop/interface/gtk-theme"),
-        DesktopEnvironment::Kde => check_file("Name=", "/home/eduardo/.config/kdeglobals"),
+        DesktopEnvironment::Kde => check_config_file("Name=", "kdeglobals"),
         DesktopEnvironment::Lxde => false,
         DesktopEnvironment::Lxqt => false,
         DesktopEnvironment::MacOs => false,
         DesktopEnvironment::Mate => check_dconf("/org/mate/desktop/interface/gtk-theme"),
         DesktopEnvironment::Unity => check_dconf("/org/gnome/desktop/interface/gtk-theme"),
         DesktopEnvironment::Windows => false,
-        DesktopEnvironment::Xfce => check_file("name=\"ThemeName\"", "/home/eduardo/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml")
+        DesktopEnvironment::Xfce => check_config_file("name=\"ThemeName\"", "xfce4/xfconf/xfce-perchannel-xml/xsettings.xml")
     }
 }
 
-fn check_file(pattern: &str, path: &str) -> bool {
+fn check_file(pattern: &str, path: &Path) -> bool {
     if let Ok(content) = std::fs::read_to_string(path) {
         let theme = content.lines().filter(|line| line.contains(pattern)).collect::<String>();
         theme.to_lowercase().contains("dark")
+    } else {
+        false
+    }
+}
+
+fn check_config_file(pattern: &str, path: &str) -> bool {
+    if let Some(config_dir) = dirs::config_dir() {
+        check_file(pattern, &config_dir.join(path))
     } else {
         false
     }
