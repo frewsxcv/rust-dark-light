05cb5c13092d732fab826f1e44ccbe0095eefeef
Be <be@mixxx.org>
2022-01-06 10:01:15 -0600

    simplify macOS implementation
    Co-authored-by: Eduardo Flores <edfloreshz@gmail.com>

--- src/macos.rs
+++ src/macos.rs
@@ -1,22 +1,15 @@
+use crate::Mode;
 use std::process;
 
-fn is_dark_mode_enabled() -> bool {
+pub fn detect() -> Mode {
     if let Ok(output) = process::Command::new("defaults")
         .arg("read")
         .arg("-g")
         .arg("AppleInterfaceStyle")
         .output()
     {
-        output.stdout.starts_with(b"Dark")
+        Mode::from(output.stdout.starts_with(b"Dark"))
     } else {
-        false
-    }
-}
-
-pub fn detect() -> crate::Mode {
-    if is_dark_mode_enabled() {
-        crate::Mode::Dark
-    } else {
-        crate::Mode::Light
+        Mode::Light
     }
 }
