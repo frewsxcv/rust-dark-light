914a494e6f63cc3d250906c9d215fb8761f2178b
Corey Farwell <coreyf@rwell.org>
2022-01-15 11:56:05 -0500

    Fetch macOS dark mode without spawning shell.
    Fixes #2.

--- src/macos.rs
+++ src/macos.rs
@@ -1,15 +1,39 @@
-use std::process;
+use objc::runtime::Object;
+use objc::{class, msg_send, sel, sel_impl};
+
+extern "C" {
+    static NSAppearanceNameAqua: *const Object;
+    static NSAppearanceNameAccessibilityHighContrastAqua: *const Object;
+    static NSAppearanceNameDarkAqua: *const Object;
+    static NSAppearanceNameAccessibilityHighContrastDarkAqua: *const Object;
+}
 
 fn is_dark_mode_enabled() -> bool {
-    if let Ok(output) = process::Command::new("defaults")
-        .arg("read")
-        .arg("-g")
-        .arg("AppleInterfaceStyle")
-        .output()
-    {
-        output.stdout.starts_with(b"Dark")
-    } else {
-        false
+    unsafe {
+        let mut appearance: *const Object = msg_send![class!(NSAppearance), currentAppearance];
+        if appearance.is_null() {
+            appearance = msg_send![class!(NSApp), effectiveAppearance];
+        }
+
+        let objects = [
+            NSAppearanceNameAqua,
+            NSAppearanceNameAccessibilityHighContrastAqua,
+            NSAppearanceNameDarkAqua,
+            NSAppearanceNameAccessibilityHighContrastDarkAqua,
+        ];
+        let names: *const Object = msg_send![
+            class!(NSArray),
+            arrayWithObjects:objects.as_ptr()
+            count:objects.len()
+        ];
+
+        let style: *const Object = msg_send![
+            appearance,
+            bestMatchFromAppearancesWithNames:&*names
+        ];
+
+        return style == NSAppearanceNameDarkAqua
+            || style == NSAppearanceNameAccessibilityHighContrastDarkAqua;
     }
 }
 
