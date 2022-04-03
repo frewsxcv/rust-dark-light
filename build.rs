bc6eeda338804244e1b91f7f5a563abfca278ca6
12101111 <w12101111@gmail.com>
2022-04-04 00:45:22 +0800

    Fix vendor detect in build script

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 fn main() {
-    #[cfg(target_vendor = "apple")]
-    {
+    if let Ok("apple") = std::env::var("CARGO_CFG_TARGET_VENDOR").as_deref() {
         println!("cargo:rustc-link-lib=framework=AppKit");
     }
 }
