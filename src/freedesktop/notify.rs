a0bab202832ce28bb820e4151dc68ff908a8e2b9
Eduardo Flores <edfloreshz@gmail.com>
2022-12-28 12:19:35 -0700

    Implemented channels for Linux notify
    - Updated ashpd

--- src/freedesktop/notify.rs
+++ src/freedesktop/notify.rs
@@ -1,8 +1,8 @@
-use std::thread;
-use async_ashpd::Error;
-use async_ashpd::desktop::settings::{SettingsProxy, ColorScheme};
-use async_ashpd::zbus::Connection;
-use crossbeam::channel::{self, Sender};
+
+use ashpd::Error;
+use ashpd::desktop::settings::{SettingsProxy, ColorScheme};
+use ashpd::zbus::Connection;
+use tokio::sync::mpsc::Sender;
 
 use crate::Mode;
 
@@ -23,28 +23,16 @@ impl<'a> Settings<'a> {
     }
 }
 
-pub async fn notify(callback: &dyn Fn(Mode)) -> anyhow::Result<()> {
-    let (tx, rx) = channel::unbounded();
+pub async fn notify(tx: Sender<crate::Mode>) -> anyhow::Result<()> {
     let connection = Connection::session().await?;
     let proxy = SettingsProxy::new(&connection).await?;
     let settings = Settings::new(&proxy);
-    if get_freedesktop_color_scheme().is_ok() {
+    if get_freedesktop_color_scheme().await.is_ok() {
         loop {
-            let tx = tx.clone();
-            freedesktop_watch(tx, &settings).await?;
-            match rx.recv() {
-                Ok(mode) => callback(mode),
-                Err(_) => {},
-            }
+            freedesktop_watch(tx.clone(), &settings).await?
         }
     } else {
-        non_freedesktop_watch(tx)?;
-        loop {
-            match rx.recv() {
-                Ok(mode) => callback(mode),
-                Err(_) => {},
-            }
-        }
+        non_freedesktop_watch(tx)
     }
 }
 
@@ -54,19 +42,19 @@ async fn freedesktop_watch<'a>(tx: Sender<Mode>, settings: &'a Settings<'a>) -> 
         ColorScheme::PreferLight => Mode::Light,
         ColorScheme::NoPreference => Mode::Default,
     };
-    tx.send(color_scheme).unwrap();
+    tx.send(color_scheme).await?;
     Ok(())
 }
 
 fn non_freedesktop_watch(tx: Sender<Mode>) -> anyhow::Result<()> {
     let mut mode = detect();
-    thread::spawn(move || {
+    tokio::spawn(async move {
         // TODO: Remove and replace for something like `notify`.
         loop {
             let new_mode = detect();
             if mode != new_mode {
                 mode = new_mode;
-                tx.send(mode).unwrap();
+                tx.send(mode).await.unwrap();
             }
         }
     });
