d718b3dc9db599f284dd1c1eed9763ae8f9214d3
Eduardo Flores <edfloreshz@gmail.com>
2022-12-30 11:19:51 -0700

    Upgraded ashpd
    - Upgraded to the master branch of ashpd
    - Moved the thread spawning to `notify`
    - Made the async methods return `anyhow::Result<T>`

--- examples/color_scheme.rs
+++ examples/color_scheme.rs
@@ -1,7 +1,6 @@
 #[tokio::main]
 async fn main() {
-    let connection = ashpd::zbus::Connection::session().await.unwrap();
-    let proxy = ashpd::desktop::settings::SettingsProxy::new(&connection).await.unwrap();
+    let proxy = ashpd::desktop::settings::Settings::new().await.unwrap();
     let color_scheme = proxy.color_scheme().await.unwrap();
     match color_scheme {
         ashpd::desktop::settings::ColorScheme::PreferDark => println!("Dark"),
