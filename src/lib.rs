26d1eb6cd07411c68b9063d5be9356450915c834
Eduardo Flores <edfloreshz@gmail.com>
2024-02-04 20:44:06 -0700

    Updated dependencies 🎁

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,14 @@ mod platforms;
 use platforms::platform;
 
 mod utils;
+#[cfg(any(
+    target_os = "linux",
+    target_os = "freebsd",
+    target_os = "dragonfly",
+    target_os = "netbsd",
+    target_os = "openbsd"
+))]
+use utils::rgb::Rgb;
 
 /// Enum representing dark mode, light mode, or unspecified.
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
@@ -38,6 +46,23 @@ impl Mode {
             Mode::Dark
         } else {
             Mode::Light
+        }
+    }
+
+    #[cfg(any(
+        target_os = "linux",
+        target_os = "freebsd",
+        target_os = "dragonfly",
+        target_os = "netbsd",
+        target_os = "openbsd"
+    ))]
+    /// Convert an RGB color to [`Mode`]. The color is converted to grayscale, and if the grayscale value is less than 192, [`Mode::Dark`] is returned. Otherwise, [`Mode::Light`] is returned.
+    fn from_rgb(rgb: Rgb) -> Self {
+        let window_background_gray = (rgb.0 * 11 + rgb.1 * 16 + rgb.2 * 5) / 32;
+        if window_background_gray < 192 {
+            Self::Dark
+        } else {
+            Self::Light
         }
     }
 }
