418381e7b550ae63b187f4ed0307c0ccf97b762e
Eduardo Flores <edfloreshz@gmail.com>
2024-02-05 03:28:50 -0700

    Added default implementaton for other platforms.

--- src/platforms/freedesktop/notify.rs
+++ src/platforms/freedesktop/notify.rs
@@ -12,6 +12,7 @@ pub struct ThemeWatcher {
     current_mode: Mutex<Mode>,
 }
 
+/// Theme watcher implementation for subscriptions.
 impl ThemeWatcher {
     pub fn new() -> Arc<Mutex<Self>> {
         let (sender, _) = broadcast::channel::<Mode>(256);
@@ -21,10 +22,8 @@ impl ThemeWatcher {
             current_mode: Mutex::new(detect()),
         };
 
-        // Wrap the ThemeWatcher in an Arc<Mutex<ThemeWatcher>> for cloning
         let arc_watcher = Arc::new(Mutex::new(theme_watcher));
 
-        // Spawn the asynchronous task
         tokio::spawn({
             let arc_watcher = Arc::clone(&arc_watcher);
             async move {
@@ -35,18 +34,18 @@ impl ThemeWatcher {
         arc_watcher
     }
 
-    // Method to get the current theme mode
+    /// Method to get the current theme mode
     pub async fn get_current_mode(&self) -> Mode {
         let current_mode = self.current_mode.lock().await;
         current_mode.clone()
     }
 
-    // Method to subscribe to theme change events
+    /// Method to subscribe to theme change events
     pub fn subscribe(&self) -> broadcast::Receiver<Mode> {
         self.sender.subscribe()
     }
 
-    // The asynchronous method to monitor theme changes
+    /// The asynchronous method to monitor theme changes
     async fn monitor_theme_changes(&self) {
         if get_freedesktop_color_scheme().await.is_ok() {
             let proxy = Settings::new().await.unwrap();
--- src/platforms/macos/notify.rs
+++ src/platforms/macos/notify.rs
@@ -1,5 +1,49 @@
-use crate::Mode;
+use std::sync::Arc;
 
-pub async fn notify(_action: fn(mode: Mode)) -> anyhow::Result<()> {
-    todo!()
+use tokio::sync::{broadcast, Mutex};
+
+use crate::{detect, Mode};
+
+pub struct ThemeWatcher {
+    sender: broadcast::Sender<Mode>,
+    current_mode: Mutex<Mode>,
+}
+
+/// Theme watcher implementation for subscriptions.
+impl ThemeWatcher {
+    pub fn new() -> Arc<Mutex<Self>> {
+        let (sender, _) = broadcast::channel::<Mode>(256);
+
+        let theme_watcher = ThemeWatcher {
+            sender,
+            current_mode: Mutex::new(detect()),
+        };
+
+        let arc_watcher = Arc::new(Mutex::new(theme_watcher));
+
+        tokio::spawn({
+            let arc_watcher = Arc::clone(&arc_watcher);
+            async move {
+                arc_watcher.lock().await.monitor_theme_changes().await;
+            }
+        });
+
+        arc_watcher
+    }
+
+    /// Method to get the current theme mode
+    pub async fn get_current_mode(&self) -> Mode {
+        let current_mode = self.current_mode.lock().await;
+        current_mode.clone()
+    }
+
+    /// Method to subscribe to theme change events
+    pub fn subscribe(&self) -> broadcast::Receiver<Mode> {
+        self.sender.subscribe()
+    }
+
+    /// The asynchronous method to monitor theme changes
+    async fn monitor_theme_changes(&self) {
+        todo!("Implement theme monitoring for macOS")
+    }
 }
