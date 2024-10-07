49c762255dd78d43c95d512022f0ded20ac10f4b
Be <be.0@gmx.com>
2024-10-06 20:33:44 -0500

    Freedesktop: use ashpd for both sync & async implementations
    Before, the sync and async APIs had completely separate
    implementations. When Linux support was first added in
    https://github.com/frewsxcv/rust-dark-light/pull/6
    the XDG Desktop Portal color-scheme API was new and not
    yet widely supported, so fallbacks were needed. Now,
    the portal API is more widely supported so the fallbacks
    are not needed. In my testing, the portal works on:
    
    KDE Plasma
    GNOME
    Cinnamon
    Xfce
    
    MATE does not currently support the portal API. Selecting
    MATE's GTK themes from Xfce's settings also does not
    change what xdg-desktop-portal-gtk reports. I am not sure,
    but I think that may be due to missing metadata in MATE's
    GTK themes. The old implementation never worked on MATE
    anyway because MATE's dark themes don't have "dark" in
    their name, so there's no regression removing it.

--- src/platforms/freedesktop/mod.rs
+++ src/platforms/freedesktop/mod.rs
@@ -1,52 +1,27 @@
-use std::str::FromStr;
-
-use anyhow::Context;
-use ini::Ini;
-
-use crate::{utils::rgb::Rgb, Mode};
-
 pub mod detect;
 pub mod notify;
 
-const MATE: &str = "/org/mate/desktop/interface/gtk-theme";
-const GNOME: &str = "/org/gnome/desktop/interface/gtk-theme";
-const CINNAMON: &str = "/org/cinnamon/desktop/interface/gtk-theme";
+use crate::Mode;
 
-fn dconf_detect(path: &str) -> Mode {
-    match dconf_rs::get_string(path) {
-        Ok(theme) => {
-            if theme.to_lowercase().contains("dark") {
-                Mode::Dark
-            } else {
-                Mode::Light
-            }
-        }
-        Err(_) => Mode::Default,
-    }
-}
+use ashpd::desktop::settings::ColorScheme as PortalColorScheme;
+use ashpd::desktop::settings::Settings as XdgPortalSettings;
 
-fn kde_detect() -> anyhow::Result<Mode> {
-    let xdg = xdg::BaseDirectories::new()?;
-    let path = xdg
-        .find_config_file("kdeglobals")
-        .context("Path not found")?;
-    let cfg = Ini::load_from_file(path)?;
-    let properties = cfg
-        .section(Some("Colors:Window"))
-        .context("Failed to get section Colors:Window")?;
-    let background = properties
-        .get("BackgroundNormal")
-        .context("Failed to get BackgroundNormal inside Colors:Window")?;
-    let rgb = Rgb::from_str(background).unwrap();
-    Ok(Mode::from_rgb(rgb))
-}
-
-impl From<ashpd::desktop::settings::ColorScheme> for Mode {
-    fn from(value: ashpd::desktop::settings::ColorScheme) -> Self {
+impl From<PortalColorScheme> for Mode {
+    fn from(value: PortalColorScheme) -> Self {
         match value {
-            ashpd::desktop::settings::ColorScheme::NoPreference => Mode::Default,
-            ashpd::desktop::settings::ColorScheme::PreferDark => Mode::Dark,
-            ashpd::desktop::settings::ColorScheme::PreferLight => Mode::Light,
+            PortalColorScheme::NoPreference => Mode::Default,
+            PortalColorScheme::PreferDark => Mode::Dark,
+            PortalColorScheme::PreferLight => Mode::Light,
         }
     }
+}
+
+pub(crate) async fn initial_value() -> Mode {
+    XdgPortalSettings::new()
+        .await
+        .unwrap()
+        .color_scheme()
+        .await
+        .unwrap()
+        .into()
 }
