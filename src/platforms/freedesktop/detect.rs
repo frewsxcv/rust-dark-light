57974f44f101a604fe7ffd9c823a9f0107936ee5
Be <be.0@gmx.com>
2024-10-06 20:33:44 -0500

    Freedesktop: use ashpd for both sync & async implementations
    Before, the sync and async APIs had completely separate
    implementations. When Linux support was first added in
    https://github.com/frewsxcv/rust-dark-light/pull/6
    the XDG Desktop Portal color-scheme API was new and not
    yet widely supported, so fallbacks were needed. Now,
    the portal API is more widely supported so the fallbacks
    are not needed. In my testing, the portal works on:
    
    KDE Plasma
    GNOME
    Cinnamon
    Xfce
    
    MATE does not currently support the portal API. Selecting
    MATE's GTK themes from Xfce's settings also does not
    change what xdg-desktop-portal-gtk reports. I am not sure,
    but I think that may be due to missing metadata in MATE's
    GTK themes. The old implementation never worked on MATE
    anyway because MATE's dark themes don't have "dark" in
    their name, so there's no regression removing it.

--- src/platforms/freedesktop/detect.rs
+++ src/platforms/freedesktop/detect.rs
@@ -1,22 +1,6 @@
-use detect_desktop_environment::DesktopEnvironment;
-
 use crate::Mode;
-
-use super::{dconf_detect, kde_detect, CINNAMON, GNOME, MATE};
+use super::initial_value;
 
 pub fn detect() -> Mode {
-    match DesktopEnvironment::detect() {
-        Some(mode) => match mode {
-            DesktopEnvironment::Kde => match kde_detect() {
-                Ok(mode) => mode,
-                Err(_) => Mode::Default,
-            },
-            DesktopEnvironment::Cinnamon => dconf_detect(CINNAMON),
-            DesktopEnvironment::Gnome => dconf_detect(GNOME),
-            DesktopEnvironment::Mate => dconf_detect(MATE),
-            DesktopEnvironment::Unity => dconf_detect(GNOME),
-            _ => Mode::Default,
-        },
-        None => Mode::Default,
-    }
+    pollster::block_on(initial_value())
 }
