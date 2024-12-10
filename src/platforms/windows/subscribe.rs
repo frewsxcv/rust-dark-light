a83915085b3fcbb869aeb9f6dd9088e1c6144f78
Eduardo Flores <edfloreshz@gmail.com>
2024-12-10 22:20:24 +0100

    windows: split sync and async api

--- src/platforms/windows/subscribe.rs
+++ src/platforms/windows/subscribe.rs
@@ -1,24 +1,39 @@
-use std::error::Error;
-use std::task::Poll;
+use crate::Mode;
 
-use futures::{stream, Stream};
+#[cfg(feature = "sync")]
+pub fn subscribe() -> std::sync::mpsc::Receiver<Mode> {
+    let (tx, rx) = std::sync::mpsc::channel();
+    let mut last_mode = crate::detect();
 
-use crate::{detect, Mode};
+    tx.send(last_mode).unwrap();
 
-pub async fn subscribe() -> Result<impl Stream<Item = Mode> + Send, Box<dyn Error>> {
-    let mut last_mode = detect();
-
-    let stream = stream::poll_fn(move |ctx| -> Poll<Option<Mode>> {
-        let current_mode = detect();
+    std::thread::spawn(move || loop {
+        let current_mode = crate::detect();
 
         if current_mode != last_mode {
+            if tx.send(current_mode).is_err() {
+                break;
+            }
             last_mode = current_mode;
-            Poll::Ready(Some(current_mode))
-        } else {
-            ctx.waker().wake_by_ref();
-            Poll::Pending
         }
     });
 
-    Ok(stream)
+    rx
 }
+
+#[cfg(not(feature = "sync"))]
+pub async fn subscribe() -> impl futures::Stream<Item = Mode> {
+    Box::pin(futures::stream::unfold(
+        crate::detect().await,
+        |last_mode| async move {
+            loop {
+                let current_mode = crate::detect().await;
+
+                if current_mode != last_mode {
+                    return Some((current_mode, current_mode));
+                }
+            }
+        },
+    ))
+}
+
